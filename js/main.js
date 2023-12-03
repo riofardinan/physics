7579a5f380c124cfffd802c805e552aa0e13dfd1
riofardinan <riofardinan123@gmail.com>
2023-12-03 13:50:44 +0700

    Physics V1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let config = {
+    type: Phasser.AUTO,
+    width: 800,
+    height: 800,
+    physics: {
+        default: 'arcade'
+    },
+    scene: []
+
+};
+let game = new Phaser.Game(config);
